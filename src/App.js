1aca0dc7f5bc8ebc0079f5f7ca2925ec4cabb2fb
aakash0519 <19uec168@lnmiit.ac.in>
2020-06-16 13:12:59 +0530

    react fetch api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       advice:''
+    }
+  }
+  componentDidMount(){
+    this.fetchAdvice();    
+  }
+  
+  fetchAdvice = () => {
+     axios.get("https://api.adviceslip.com/advice")
+      .then((res)=>{
+      const {advice} = res.data.slip;
+        this.setState({advice});
+        
+      })
+      .catch((err) => {
+        console.log(err);
+        
+      });
+  }
+
+  render() {
+    return (
+      <>
+       <div className = "app">    
+         <div className="card">
+           <h1 className="heading">{this.state.advice}</h1>
+           <button id = "butt" onClick ={this.fetchAdvice}>
+             <span>GIVE ME ADVICE !</span>
+           </button>
+         </div>
+       </div>
+       </>
+    )
+  }
 }
 
-export default App;
+export default App
